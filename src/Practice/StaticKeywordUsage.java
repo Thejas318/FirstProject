dc154de8a518f0ffeea2dc6d3e373efd68ca9c5d
Thejas318 <tejasgowda.ys8@gmail.com>
2022-10-28 00:08:40 -0400

    few changes

--- src/Practice/StaticKeywordUsage.java
+++ src/Practice/StaticKeywordUsage.java
@@ -1,2 +1,32 @@
-package Practice;public class StaticKeywordUsage {
+package Practice;
+class Emp{
+
+    int empid;
+    int salary;
+    static String ceo;
+
+    static {            // static method.
+        ceo = "Arman"; // initializing the static variable. This will be called only once and will make changes in all the objects.
+        System.out.println("called only once");
+    }
+    Emp(){           // Constructor method.
+        empid = 1;   // constructor method can be used to intialize the non-static variables.
+        salary = 800; // constructor method will be called only once per object.
+        System.out.println("called once per obect");
+    }
+    public void show(){
+        System.out.println( empid  + ":" + salary + ":" + ceo);
+    }
+
+}
+public class StaticKeywordUsage {
+    public static void main(String[] args) {
+    Emp thejas = new Emp();
+    Emp sriram = new Emp();
+
+
+
+    thejas.show();
+    sriram.show();
+    }
 }
