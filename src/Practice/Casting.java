dc2364acb112388c8cbde99aa0910bb13655bbe8
Thejas318 <tejasgowda.ys8@gmail.com>
2022-11-03 01:53:30 -0400

    new

---
+++ src/Practice/Casting.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Casting {
+
+    public static void main(String[] args) {
+
+        int x = (int) Math.round(1222.49); //Round method returns the long datatype output.
+                                           // By casting, it can assigned to the int type variable.
+        System.out.println(x);
+
+
+    }
+}
