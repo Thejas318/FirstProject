9063ec4b7d8ffc32b871aeed6cb10a7048232429
Thejas318 <tejasgowda.ys8@gmail.com>
2022-11-05 11:42:56 -0400

    This is to learn jenkins

--- src/Practice/Modulus.java
+++ src/Practice/Modulus.java
@@ -4,5 +4,6 @@ public class Modulus {
     public static void main(String[] args) {
         int x = 10003 % 10;
         System.out.println(x);
+        System.out.println("This is to connect to jenkins");
     }
 }
